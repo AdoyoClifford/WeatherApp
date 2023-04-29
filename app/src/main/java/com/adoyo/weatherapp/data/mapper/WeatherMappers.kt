87fc3adbfb4da015cc3782679d1f9efc5a3b7926
Adoyo Clifford <adoyoclifford@gmail.com>
2023-04-29 04:03:34 +0000

    initial commit

--- app/src/main/java/com/adoyo/weatherapp/data/mapper/WeatherMappers.kt
+++ app/src/main/java/com/adoyo/weatherapp/data/mapper/WeatherMappers.kt
@@ -1,8 +1,11 @@
 package com.adoyo.weatherapp.data.mapper
 
 import android.os.Build
+import androidx.annotation.RequiresApi
 import com.adoyo.weatherapp.data.remote.WeatherDataDto
+import com.adoyo.weatherapp.data.remote.WeatherDto
 import com.adoyo.weatherapp.domain.weather.WeatherData
+import com.adoyo.weatherapp.domain.weather.WeatherInfo
 import com.adoyo.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,6 +13,7 @@ import java.time.format.DateTimeFormatter
 
 private data class IndexWeatherData(val index: Int, val data: WeatherData)
 
+@RequiresApi(Build.VERSION_CODES.O)
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
     return time.mapIndexed { index, time ->
         val temperatures = temperatures[index]
@@ -17,19 +21,31 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
         val pressure = pressures[index]
         val windSpeed = windSpeeds[index]
         val humidity = humidities[index]
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            IndexWeatherData(
-                index = index, data = WeatherData(
-                    time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
-                    temperatureCelsius = temperatures,
-                    pressure = pressure,
-                    windSpeed = windSpeed,
-                    humidity = humidity,
-                    weatherType = WeatherType.fromWMO(weatherCodes)
-                )
+
+        IndexWeatherData(
+            index = index, data = WeatherData(
+                time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
+                temperatureCelsius = temperatures,
+                pressure = pressure,
+                windSpeed = windSpeed,
+                humidity = humidity,
+                weatherType = WeatherType.fromWMO(weatherCodes)
             )
-        } else {
-            TODO("VERSION.SDK_INT < O")
-        }
+        )
+
     }.groupBy { it.index / 24 }.mapValues { it.value.map { it.data } }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find {
+        val hour = if (now.minute < 30) now.hour else now.hour + 1
+        it.time.hour == hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
 }
