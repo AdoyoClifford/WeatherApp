ea0fe7619f4f83f458e51254f694fff5da41622c
Adoyo Clifford <adoyoclifford@gmail.com>
2023-04-29 11:39:00 +0000

    initial commit

--- app/src/main/java/com/adoyo/weatherapp/presentation/WeatherCard.kt
+++ app/src/main/java/com/adoyo/weatherapp/presentation/WeatherCard.kt
@@ -3,7 +3,9 @@ package com.adoyo.weatherapp.presentation
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,10 +19,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.adoyo.weatherapp.R
 import java.time.format.DateTimeFormatter
+import kotlin.math.roundToInt
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -65,6 +71,29 @@ fun WeatherCard(
                     color = Color.White
                 )
                 Spacer(modifier = Modifier.height(32.dp))
+                Row(
+                   modifier = Modifier.fillMaxWidth(),
+                     horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherDataDisplay(
+                        value = data.pressure.roundToInt(),
+                        unit = "hpa",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_pressure),
+                        iconTint = Color.White
+                    )
+                    WeatherDataDisplay(
+                        value = data.humidity.roundToInt(),
+                        unit = "%",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_drop),
+                        iconTint = Color.White
+                    )
+                    WeatherDataDisplay(
+                        value = data.windSpeed.roundToInt(),
+                        unit = "km/h",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_pressure),
+                        iconTint = Color.White
+                    )
+                }
 
             }
         }
